f2c6e7e29d30dbdc50b868b8294585f42e7bfe32
Timo Taglieber <github@timotaglieber.de>
2016-04-23 21:15:44 +0200

    First draft of scoreForPlayer

--- client/js/components/score_board.es6.js
+++ client/js/components/score_board.es6.js
@@ -22,18 +22,61 @@ const Head = React.createClass({
 });
 
 const Body = React.createClass({
-  renderRow: function(player, index) {
-    // TODO: extract rolls for one player from frames array!
-    let frames = [
-      [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9],
-      [10, 10]
-    ];
-    let cells = frames.map(this.renderFrameCell);
-    // frames = [[4,5], [9]]
-    // all rolls only for that player
+  /*
+   * Extract all rolls for one player from frames array.
+   */
+  framesForPlayer: function(all_frames, player_index, max_frames) {
+    let frames = all_frames.map((frame) => frame[player_index]);
 
-    // TODO: calc final score
-    let final_score = 90;
+    // Pad empty frames to maintain row length equal in table.
+    let num_empty_frames = max_frames - frames.length;
+    let empty_frames = Array.from({length: num_empty_frames}).map(() => []);
+
+    return frames.concat(empty_frames);
+  },
+
+  /*
+   * Calculate the current score for one player.
+   */
+  scoreForPlayer: function(frames, max_frames, max_pins) {
+
+    // Flatten array of frames
+    let rolls = [].concat.apply([], frames);
+
+    // Initialize multipliers
+    let multipliers = rolls.slice().map(() => 1);
+
+    // Account for strikes in frames 1-9 (skip 10th)
+    rolls.slice(0, max_frames - 1).forEach((roll, idx) => {
+      if (roll === max_pins) {
+        // Strike detected.
+        // Increase the multipliers for the next two rolls, if present.
+        if (idx < rolls.length - 1) {
+          multipliers[idx + 1]++;
+          if (idx < rolls.length - 2) {
+            multipliers[idx + 2]++;
+          }
+        }
+      }
+    });
+
+    console.log(multipliers);
+
+    // frames  [1       2       3       4       5       6       7       8       9       10]
+    // indexes [0,  1,  2,  3,  4,  5]
+    // rolls = [4,  6,  1, 10,  4,  6,  1,  2,  1,  2,  1,  2,  1,  2,  1,  2,  1,  2,  5,  5,  3]
+    // multi = [1,  1,  2,  1,  2,  2,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  1]
+    return rolls.reduce((res, curr, index) => {
+      return res + curr * multipliers[index]
+    }, 0);
+  },
+
+  renderRow: function(player, index) {
+    let frames = this.framesForPlayer(this.props.frames, index, Constants.MAX_FRAMES);
+    let cells = frames.map(this.renderFrameCell);
+    let final_score = this.scoreForPlayer(
+      frames, Constants.MAX_FRAMES, Constants.MAX_PINS
+    );
     return (
       <tr key={index}>
         <td>{player}</td>
@@ -45,17 +88,20 @@ const Body = React.createClass({
 
   renderFrameCell: function(rolls, index) {
     // rolls = [], [5], [4,5]
+    let scores = rolls.map((roll, idx) => (
+      <span key={idx} className="roll-score">{roll}</span>
+    ));
     return (
-      <td key={index}>{JSON.stringify(rolls)}</td>
+      <td key={index}>
+        {scores}
+      </td>
     );
   },
 
   render: function() {
-    let rows = this.props.players.map(this.renderRow);
-
     return (
       <tbody>
-        {rows}
+        {this.props.players.map(this.renderRow)}
       </tbody>
     );
   }
