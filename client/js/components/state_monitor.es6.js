f1226679b3c3a28b8945f0347db7166acf625e4f
Timo Taglieber <github@timotaglieber.de>
2016-04-24 15:58:54 +0200

    Prettier JSON formatting for state monitor

--- client/js/components/state_monitor.es6.js
+++ client/js/components/state_monitor.es6.js
@@ -3,15 +3,31 @@ import { connect } from 'react-redux';
 
 const Component = React.createClass({
   render: function() {
+    let started = JSON.stringify(this.props.started);
+    let players = JSON.stringify(this.props.players);
+    let frames = JSON.stringify(this.props.frames);
+    let ended = JSON.stringify(this.props.ended);
     return (
       <div id="state_mmonitor">
         <h2>State monitor</h2>
-        <pre>{ JSON.stringify(this.props.game) }</pre>
+        <pre>
+          {'state = {\n'}
+          {'  started: ' + started + ',\n'}
+          {'  players: ' + players + ',\n'}
+          {'  frames:  ' + frames + ',\n'}
+          {'  ended:   ' + ended + '\n'}
+          {'}'}
+        </pre>
       </div>
     );
   }
 });
 
-const mapStateToProps = (state) => ({game: state});
+const mapStateToProps = (state) => ({
+  started: state.started,
+  players: state.players,
+  frames: state.frames,
+  ended: state.ended
+});
 const StateMonitor = connect(mapStateToProps)(Component);
 export default StateMonitor;
